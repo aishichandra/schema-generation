da5e01903ff53218e7d059d54a30bed7b1ea3076
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-11-08 17:01:36 -0600

    fix OCR output format

--- ocr_docling.py
+++ ocr_docling.py
@@ -1,19 +1,17 @@
-import json
-
 from docling.document_converter import DocumentConverter
 
 source = "./data/campaign_finance_sample.pdf"
 converter = DocumentConverter()
 result = converter.convert(source)
-result_json = result.document.export_to_dict()
+result_md = result.document.export_to_markdown()
 
-with open("./data/eval_outputs/campaign_finance_ocr.json", "w") as f:
-    json.dump(result_json, f, indent=2)
+with open("./data/eval_outputs/campaign_finance_ocr.md", "w") as f:
+    f.write(result_md)
 
 source = "./data/poll.pdf"
 converter = DocumentConverter()
 result = converter.convert(source)
-result_json = result.document.export_to_dict()
+result_md = result.document.export_to_markdown()
 
-with open("./data/eval_outputs/poll_ocr.json", "w") as f:
-    json.dump(result_json, f, indent=2)
+with open("./data/eval_outputs/poll_ocr.md", "w") as f:
+    f.write(result_md)
