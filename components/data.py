5da4b5ca944f1fc85f090bf3a52f1c78d94c91d6
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-11-25 14:39:02 -0600

    refactor data extraction

---
+++ components/data.py
@@ -0,0 +1,32 @@
+import json
+
+import streamlit as st
+from schema_flow import extract_data_with_schema, get_schema_class
+
+
+def extract_data(n_selected):
+    if st.button("Extract Data", key="extract_data_button", disabled=n_selected == 0):
+        schema_code = st.session_state.schema
+        if schema_code is not None:
+            schema_class, _ = get_schema_class(schema_code)
+            # Extract data
+            selected_pages = [
+                st.session_state.pages[i] for i in st.session_state.selected_pages
+            ]
+            data = extract_data_with_schema(selected_pages, schema_class)
+            st.session_state.extracted_data = data
+            st.success("Data extracted successfully.")
+        else:
+            st.error("Please generate or provide a schema first.")
+
+
+def download_data():
+    if st.session_state.extracted_data is not None:
+        data = st.session_state.extracted_data
+        json_data = json.dumps(data, indent=2)
+        st.download_button(
+            label="Download Extracted Data",
+            data=json_data,
+            file_name="data.json",
+            mime="application/json",
+        )
--- ui_schema.py
+++ ui_schema.py
@@ -1,12 +1,7 @@
-import json
-
 import streamlit as st
 
+from components.data import download_data, extract_data
 from components.files import file_uploader, page_selector
-from components.schema_flow import (
-    extract_data_with_schema,
-    get_schema_class,
-)
 from components.schemas import schema_interface
 from components.state import initialize_state
 
@@ -48,27 +43,7 @@ if st.session_state.pages is not None:
     schema_interface(n_selected)
 
     # Extract data button
-    if st.button("Extract Data", key="extract_data_button", disabled=n_selected == 0):
-        schema_code = st.session_state.schema
-        if schema_code is not None:
-            schema_class, _ = get_schema_class(schema_code)
-            # Extract data
-            selected_pages = [
-                st.session_state.pages[i] for i in st.session_state.selected_pages
-            ]
-            data = extract_data_with_schema(selected_pages, schema_class)
-            st.session_state.extracted_data = data
-            st.success("Data extracted successfully.")
-        else:
-            st.error("Please generate or provide a schema first.")
+    extract_data(n_selected)
 
     # If data has been extracted, allow user to download it as JSON
-    if st.session_state.extracted_data is not None:
-        data = st.session_state.extracted_data
-        json_data = json.dumps(data, indent=2)
-        st.download_button(
-            label="Download Extracted Data",
-            data=json_data,
-            file_name="data.json",
-            mime="application/json",
-        )
+    download_data()
